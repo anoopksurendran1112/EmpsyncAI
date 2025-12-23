e4504914cb03c4f6dda13e1440cce0fd12caf436
Anoop <anoopksurendran2002@gmail.com>
2025-12-23 09:01:45 +0530

    feat: Created add past leave API at the backend

--- backend/leave/urls.py
+++ backend/leave/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import get_calendar,apply_leave,get_leave_types,get_requested_leaves,update_leave_status,update_leave_type,get_holiday,update_holiday,add_holiday
+from .views import get_calendar,apply_leave,get_leave_types,get_requested_leaves,update_leave_status,update_leave_type,get_holiday,update_holiday,add_holiday, add_past_leave
 
 urlpatterns = [path('api/get-calendar/<int:id>',get_calendar),
                 path('api/apply-leave',apply_leave),
+                path('api/add-leave', add_past_leave),
                 path('api/leave-types',get_leave_types),
                 path('api/admin/leave-request/<int:page>',get_requested_leaves),
                 path('api/update-leave',update_leave_status),
